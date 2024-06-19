0268614f6dd03e73a6d177569e15c5b5f00e11b6
WaseqAlvi <dynamoalvi@gmail.com>
2024-06-19 10:08:00 +0600

    adding files

---
+++ src/main/java/com/login/Login/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.login.Login.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping ("/")
+    public  String home(){
+        return "hello";
+    }
+    @GetMapping ("/secured")
+    public String secured(){
+        return "Secured";
+    }
+}
